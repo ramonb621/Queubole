0faabf292f9b26bdfd5f2e92970c005de9be89ae
Ramon Barajas <ramonb621@yahoo.com>
2020-07-12 17:14:30 -0700

    adding routes folder, modifying connection...

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 let map, infoWindow;
-initMap = () =>{
+const initMap = () =>{
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 6
